b0c14cc43a5a96c5939f6b0f0bf2c571c327de9b
Jimmy Forrester-Fellowes <jimmy@rocketware.co.uk>
2018-07-28 16:25:37 +0100

    Dart 2 support and tidy up

--- example/example.dart
+++ example/example.dart
@@ -1,47 +1,48 @@
+import 'dart:async';
 import 'dart:html';
 import 'dart:typed_data';
-import 'dart:async' as async;
+
 import "package:gifencoder/gifencoder.dart" as gifencoder;
 
-const size = 200;
-const frameCount = 32;
-const framesPerSecond = 15;
+const int size = 200;
+const int frameCount = 32;
+const int framesPerSecond = 15;
 
-drawSquare(CanvasRenderingContext2D ctx, int frameNumber) {
-  var frameFraction = (frameNumber / frameCount);
-  var green = 0;
-  var blue = 0;
-  for (var i = 0; i < size/2; i++) {
-    var drawnFraction = i / (size/2);
-    var red = (256 * (frameFraction + drawnFraction)).floor() % 256;
-    ctx.fillStyle = "rgb(${red},${green},${blue})";  
-    ctx.fillRect(i, i, size - i*2, size - i*2);  
-  }  
+void drawSquare(CanvasRenderingContext2D ctx, int frameNumber) {
+  final frameFraction = (frameNumber / frameCount);
+  final green = 0;
+  final blue = 0;
+  for (int i = 0; i < size / 2; i++) {
+    final drawnFraction = i / (size / 2);
+    final red = (256 * (frameFraction + drawnFraction)).floor() % 256;
+    ctx
+      ..fillStyle = "rgb($red,$green,$blue)"
+      ..fillRect(i, i, size - i * 2, size - i * 2);
+  }
 }
 
-async.Future<String> createDataUrl(Uint8List bytes) {
-  var c = new async.Completer();
-  var f = new FileReader();
+Future<String> createDataUrl(Uint8List bytes) {
+  final c = new Completer<String>();
+  final f = new FileReader();
   f.onLoadEnd.listen((ProgressEvent e) {
     if (f.readyState == FileReader.DONE) {
-      String url = f.result;
+      final String url = f.result;
       c.complete(url.replaceFirst("data:;", "data:image/gif;"));
-    }    
+    }
   });
   f.readAsDataUrl(new Blob([bytes]));
   return c.future;
 }
 
-main() {
-  var ctx = new CanvasElement(width: size, height: size).context2D;
-  var frames = new gifencoder.GifBuffer(size, size);
+void main() {
+  final ctx = new CanvasElement(width: size, height: size).context2D;
+  final frames = new gifencoder.GifBuffer(size, size);
   for (var i = 0; i < frameCount; i++) {
     drawSquare(ctx, i);
     frames.add(ctx.getImageData(0, 0, size, size).data);
   }
-  Uint8List gif = frames.build(framesPerSecond);
+  final Uint8List gif = frames.build(framesPerSecond);
   createDataUrl(gif).then((dataUrl) {
-    ImageElement elt = query("#gif");
-    elt.src = dataUrl;  
+    (querySelector("#gif") as ImageElement)..src = dataUrl;
   });
 }
--- test/gif_web_test.dart
+++ test/gif_web_test.dart
@@ -1,42 +1,46 @@
+@TestOn("browser")
+
 library gif_web_test;
 
-import "package:gifencoder/gifencoder.dart" as gif;
-import 'package:unittest/unittest.dart';
-import 'package:unittest/html_config.dart';
 import 'dart:html';
 
-main() {
-  useHtmlConfiguration();
+import "package:gifencoder/gifencoder.dart" as gif;
+import 'package:test/test.dart';
 
+void main() {
   test('one black pixel', () {
-    var ctx = new CanvasElement(width: 1, height: 1).context2D ..fillStyle = "black" ..fillRect(0, 0, 1, 1);
-    var data = ctx.getImageData(0, 0, 1, 1);
-    var blob = new Blob([gif.makeGif(data.width, data.height, data.data)]);
+    final ctx = new CanvasElement(width: 1, height: 1).context2D
+      ..fillStyle = "black"
+      ..fillRect(0, 0, 1, 1);
+    final data = ctx.getImageData(0, 0, 1, 1);
+    final blob = new Blob([gif.makeGif(data.width, data.height, data.data)]);
 
-    var f = new FileReader();
+    final f = new FileReader();
 
-    loadedDataUrl(ProgressEvent e) {
+    void loadedDataUrl(ProgressEvent e) {
       expect(f.error, null);
       expect(f.readyState, FileReader.DONE);
       String url = f.result;
       expect(url, startsWith("data:;"));
       url = url.replaceFirst("data:;", "data:image/gif;");
-      var img = new ImageElement(src: url);
-      img.onLoad.listen(expectAsync((e) {
+      final img = new ImageElement(src: url);
+      img.onLoad.listen(expectAsync1((e) {
         expect(img.width, 1);
         expect(img.height, 1);
         checkPixel(img, 0, 0, [0, 0, 0, 255]);
       }));
     }
 
-    f.onLoadEnd.listen(expectAsync(loadedDataUrl));
+    f.onLoadEnd.listen(expectAsync1(loadedDataUrl));
     f.readAsDataUrl(blob);
   });
 }
 
-checkPixel(ImageElement img, int x, int y, List<int> expectedRGBA) {
-  var ctx = new CanvasElement(width: x + 1, height: y + 1).context2D ..fillStyle = "white" ..fillRect(0, 0, x + 1, y + 1);
-  ctx.drawImage(img, 0, 0);
-  var data = ctx.getImageData(0, 0, 1, 1).data;
+void checkPixel(ImageElement img, int x, int y, List<int> expectedRGBA) {
+  final ctx = new CanvasElement(width: x + 1, height: y + 1).context2D
+    ..fillStyle = "white"
+    ..fillRect(0, 0, x + 1, y + 1)
+    ..drawImage(img, 0, 0);
+  final data = ctx.getImageData(0, 0, 1, 1).data;
   expect(data, expectedRGBA);
 }
